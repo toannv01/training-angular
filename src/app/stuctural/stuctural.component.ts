e4526e82f01b42e4656b17860b9e146b1b4e7e16
Toannv <toannv@runsystem.net>
2019-08-29 11:25:09 +0700

    NgFor

--- src/app/stuctural/stuctural.component.ts
+++ src/app/stuctural/stuctural.component.ts
@@ -11,8 +11,50 @@ export class StucturalComponent implements OnInit {
   isShow = true;
   username = 'toannv';
   isChecked = false;
-  address = "ha noi";
+  address = 'ha noi';
+  names: string [] = [
+    'TPHCM', 'ha noi'
+  ];
   age = 19;
+  dataServer: string [] = [
+    'TPHCM', 'ha noi', 'Hạ long', 'Dak Lak'
+  ];
+
+  public products: any[] = [
+
+  ];
+  public productsServer: any [] = [
+    {
+      id : 1 ,
+      name : 'Sam sung',
+      // image : 'https://didongviet.vn/pub/media/catalog/product//s/a/samsung-galaxy-a70-didongviet_1_1.jpg'
+      price : 8000,
+    },
+    {
+      id : 2 ,
+      name : 'iphonge',
+      // image : 'https://didongviet.vn/pub/media/catalog/product//s/a/samsung-galaxy-a70-didongviet_1_1.jpg'
+      price : 8000,
+    },
+    {
+      id : 3 ,
+      name : 'oppoo',
+      // image : 'https://didongviet.vn/pub/media/catalog/product//s/a/samsung-galaxy-a70-didongviet_1_1.jpg'
+      price : 8000,
+    },
+    {
+      id : 4 ,
+      name : 'oppoo 7',
+      // image : 'https://didongviet.vn/pub/media/catalog/product//s/a/samsung-galaxy-a70-didongviet_1_1.jpg'
+      price : 8000,
+    },
+    {
+      id : 5 ,
+      name : 'oppoo 9',
+      // image : 'https://didongviet.vn/pub/media/catalog/product//s/a/samsung-galaxy-a70-didongviet_1_1.jpg'
+      price : 8000,
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
@@ -24,5 +66,8 @@ export class StucturalComponent implements OnInit {
   }
   onChange(value) {
     this.isChecked = value;
+  }
+  onLoadData() {
+    this.products = this.productsServer;
   }
 }
